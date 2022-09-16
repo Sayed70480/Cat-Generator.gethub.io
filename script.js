cc2f6b3543a4f50ee4ca4b7677c4e7103955bb2a
Syed sikandar Shah <syedsikandars4@gmail.com>
2022-09-16 21:11:41 +0500

    cat generator
    how to generate cat in programming

---
+++ script.js
@@ -0,0 +1,7 @@
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-box-gen');
+    image.src = "images/cat.jpg";
+    div.appendChild(image);
+
+}
